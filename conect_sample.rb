11f833169baf5a7f89b18fb5fffa47096c412413
QichiroTani <tani@solairo.co.jp>
2017-10-05 12:27:52 +0900

    Update conect_sample.rb

--- conect_sample.rb
+++ conect_sample.rb
@@ -1,6 +1,6 @@
 require 'td'
 require 'td-client'
-cln = TreasureData::Client.new('8945/f323db81493d1eee98e4f363e9124df4fbaa0dd8')
+cln = TreasureData::Client.new('********************')
 cln.databases.each { |db|
   db.tables.each { |tbl|
     p tbl.db_name
